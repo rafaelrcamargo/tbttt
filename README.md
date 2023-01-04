22502a0c678aac815a157920ede4b19e4699fd2b
rafaelrcamargo <rafaelrakochinski@gmail.com>
2023-01-04 15:02:49 -0300

    feat: better code, really better but not so much...

---
+++ README.md
@@ -0,0 +1,10 @@
+# TbTTT - Terminal based Tic-Tac-Toe
+
+I mean, why not?
+
+> Just a study of terminal based apps in rust, nothing too complex around here.
+
+#### TODO:
+
+- Fix valid input checking in `move` function.
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::io;
 
+enum Status {
+    Undefined,
+    Draw,
+    Win(usize),
+}
+
 fn main() {
     // Global
     let stdin = io::stdin();
@@ -8,7 +14,7 @@ fn main() {
     // State
     let mark = vec![" \x1b[32m\x1b[0m ", " \x1b[31m﯇\x1b[0m "];
     let mut grid = vec![
-        ["┌", "───", "───", "T", "bTT", "T", "───", "───", "┐"],
+        ["┌", "───", "── ", "T", "bTT", "T", " ──", "───", "┐"],
         ["│", "   ", "   ", " ", "   ", " ", "   ", "   ", "│"],
         ["1", "   ", "   ", "║", "   ", "║", "   ", "   ", "│"],
         ["│", "   ", "═══", "╬", "═══", "╬", "═══", "   ", "│"],
@@ -21,65 +27,39 @@ fn main() {
 
     let mut turn = 0;
     while turn <= 9 {
-        // Check if there is a winner
-        for i in 0..3 {
-            // get the row
-            let row = [grid[i * 2 + 2][2], grid[i * 2 + 2][4], grid[i * 2 + 2][6]];
+        draw_grid(&grid);
 
-            // get the column
-            let col = vec![grid[2][i * 2 + 2], grid[4][i * 2 + 2], grid[6][i * 2 + 2]];
+        let status = if turn == 9 {
+            Some(Status::Draw)
+        } else {
+            check_status(&grid)
+        };
 
-            // get the diagonal
-            let diag = vec![grid[2][i * 2 + 2], grid[4][4], grid[6][6 - i * 2]];
-
-            // Check if there is a winner, present in the mark vector
-            if row.iter().all(|&x| x == row[2]) && mark.contains(&row[2]) {
-                println!("\n{} \x1b[33mWINS!\x1b[0m 🥳", row[2].trim());
-                return;
-            } else if col.iter().all(|&x| x == col[2]) && mark.contains(&col[2]) {
-                println!("\n{} \x1b[33mWINS!\x1b[0m 🥳", col[2].trim());
-                return;
-            } else if diag.iter().all(|&x| x == diag[2]) && mark.contains(&diag[2]) {
-                println!("\n{} \x1b[33mWINS!\x1b[0m 🥳", diag[2].trim());
-                return;
+        match status {
+            Some(Status::Draw) => {
+                println!("That's a draw! 🫥");
+                break;
             }
-        }
-
-        draw(&grid);
-
-        if turn == 9 {
-            break;
+            Some(Status::Win(player)) => {
+                println!("Player {} wins! 🎉", mark[player]);
+                break;
+            }
+            _ => {}
         }
 
         // Read a line from stdin and parse to CHAR
-        println!("\n{}'s turn: ", mark[turn & 1].trim());
+        println!("{}'s turn: ", mark[turn & 1].trim());
+
         let mut input = String::new();
         stdin.read_line(&mut input).expect("Failed to read line");
-        let mut input = input.trim().chars();
-
-        let col = match input.next().unwrap() {
-            'a' => 2,
-            'b' => 4,
-            'c' => 6,
-            _ => {
-                panic!("Invalid input")
-            }
-        };
-
-        let row = match input.next().unwrap() {
-            '1' => 2,
-            '2' => 4,
-            '3' => 6,
-            _ => {
-                panic!("Invalid input")
-            }
-        };
+        let (row, col) = read_move(input);
 
         // Check if the cell is empty
         if grid[row][col] == "   " {
             grid[row][col] = mark[turn & 1];
         } else {
-            println!("Cell is not empty");
+            println!("\n> Cell is not empty! Try again.");
+            std::thread::sleep(std::time::Duration::from_millis(1000));
             turn -= 1;
         }
 
@@ -88,7 +68,7 @@ fn main() {
     }
 }
 
-fn draw(grid: &[[&str; 9]]) {
+fn draw_grid(grid: &[[&str; 9]]) {
     print!("\x1B[2J"); // Clear screen
 
     // Grid
@@ -98,4 +78,41 @@ fn draw(grid: &[[&str; 9]]) {
     });
 
     println!() // Padding
+}
+
+fn read_move(input: String) -> (usize, usize) {
+    let coord: Vec<usize> = input
+        .trim()
+        .split("")
+        .map(|pos| match pos {
+            "a" | "1" => 2usize,
+            "b" | "2" => 4usize,
+            "c" | "3" => 6usize,
+            _ => 0usize,
+        })
+        .collect();
+
+    return (coord[2], coord[1]);
+}
+
+fn check_status(grid: &[[&str; 9]]) -> Option<Status> {
+    let mark = vec![" \x1b[32m\x1b[0m ", " \x1b[31m﯇\x1b[0m "];
+    let mut status = Some(Status::Undefined);
+
+    for i in 0..3 {
+        let row = [grid[i * 2 + 2][2], grid[i * 2 + 2][4], grid[i * 2 + 2][6]];
+        let col = vec![grid[2][i * 2 + 2], grid[4][i * 2 + 2], grid[6][i * 2 + 2]];
+        let diag = vec![grid[2][i * 2 + 2], grid[4][4], grid[6][6 - i * 2]];
+
+        if (row.iter().all(|&x| x == row[2]) && mark.contains(&row[2]))
+            || (col.iter().all(|&x| x == col[2]) && mark.contains(&col[2]))
+            || (diag.iter().all(|&x| x == diag[2]) && mark.contains(&diag[2]))
+        {
+            status = Some(Status::Win(
+                mark.iter().position(|&x| x == diag[2]).unwrap(),
+            ));
+        }
+    }
+
+    status
 }
